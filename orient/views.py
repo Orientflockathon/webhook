6516c99a514242b1b1ae6bbb7280eae2fff27f37
vishrutkohli <kohlivishrut@gmail.com>
2017-04-01 21:34:47 +0530

    aa

--- orient/views.py
+++ orient/views.py
@@ -35,19 +35,19 @@ def listenEvent(request):
 			# 	"profileImage":"http://i.imgur.com/pAKIUxV.jpg"
 
 			# 	}
-	payload={
-	"to":"g:c38413fd823545f6af4e445452c36d6e",
-	"text":"Shivam is out of Bitcap \n thanks and regards",
-	"token":"67c8351f-32c7-487b-b6c3-de725daea30c",
-	# "sendAs":json.dumps(d)
-	}
-	headers={
-	"Content-Type":"application/x-www-form-urlencoded",
-	"Content-Length":"70"
-	}
-	r=requests.post(url,data=payload,headers=headers)
-	print r
-	print r.text
+		payload={
+		"to":"g:c38413fd823545f6af4e445452c36d6e",
+		"text":"Shivam is out of Bitcap \n thanks and regards",
+		"token":"67c8351f-32c7-487b-b6c3-de725daea30c",
+		# "sendAs":json.dumps(d)
+		}
+		headers={
+		"Content-Type":"application/x-www-form-urlencoded",
+		"Content-Length":"70"
+		}
+		r=requests.post(url,data=payload,headers=headers)
+		print r
+		print r.text
 
 
 	
