efd5654bcdb0c52b2d7f026a980c3a58f2ea76d2
vishrutkohli <kohlivishrut@gmail.com>
2017-04-01 19:23:32 +0530

    aa

--- orient/admin.py
+++ orient/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from orient.models import data , Saved , Users , login
 
 # Register your models here.
+
+admin.site.register(data)
+admin.site.register(Saved)
+admin.site.register(Users)
+admin.site.register(login)
